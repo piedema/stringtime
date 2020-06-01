ce97d6e910b8767b9d17ed425ad58815b2443932
piedema <60652568+piedema@users.noreply.github.com>
2020-06-01 21:22:27 +0200

    added functions

--- stringtime.js
+++ stringtime.js
@@ -16,7 +16,7 @@ const timeframes = {
 }
 
 module.exports.toString = function(ms){
-  let string = {};
+  let string = '';
   ms = parseInt(ms);
 
   if(!ms) return ERROR.INVALID_INPUT;
@@ -24,40 +24,87 @@ module.exports.toString = function(ms){
   for(let key in timeframes){
     let stringtime = Math.floor(ms / timeframes[key]);
     if(stringtime > 0){
-      string[key] = stringtime;
-      ms = ms - string[key] * timeframes[key];
+      if(string.length > 0) string += ' ';
+      string += stringtime + key;
+      ms = ms - stringtime * timeframes[key];
     }
   }
   return string;
 }
 
-module.exports.toMs = function(strings){
+module.exports.toObject = function(ms){
+  let obj = {};
+  ms = parseInt(ms);
+
+  if(!ms) return ERROR.INVALID_INPUT;
+
+  for(let key in timeframes){
+    let stringtime = Math.floor(ms / timeframes[key]);
+    if(stringtime > 0){
+      obj[key] = stringtime;
+      ms = ms - obj[key] * timeframes[key];
+    }
+  }
+  return obj;
+}
+
+module.exports.toArray = function(ms){
+  let array = Array.apply(undefined, Array(10)).map(() => { return 0 })
+  ms = parseInt(ms);
+
+  if(!ms) return ERROR.INVALID_INPUT;
+
+  let timeframeIndex = 0
+
+  for(let key in timeframes){
+    let stringtime = Math.floor(ms / timeframes[key]);
+    if(stringtime > 0){
+      array[timeframeIndex] = stringtime;
+      ms = ms - stringtime * timeframes[key];
+    }
+    timeframeIndex++
+  }
+  return array;
+}
+
+module.exports.toMs = function(time){
   let ms = 0;
 
-  if(Array.isArray(strings)){
-    for(let i = 0; i < strings.length; i++){
-      let splitted = stringSplit(strings[i]);
+  if(Array.isArray(time)){
+    for(let i = 0; i < time.length; i++){
+      let splitted = stringSplit(time[i]);
       if(!splitted) return ERROR.INVALID_INPUT;
       ms += splitted.t * timeframes[splitted.tf];
     }
   }
 
-  if(typeof strings === 'string'){
-    let splitted = stringSplit(strings);
-    ms = timeframes[splitted.tf] ? splitted.t * timeframes[splitted.tf] : false;
+  if(typeof time === 'object' && typeof time !== null){
+    for(let t in time){
+      if(timeframes[t]) ms += timeframes[t] * time[t]
+    }
   }
+
+  if(typeof time === 'string'){
+    let times = time.split(' ')
+    for(let i = 0; i < times.length; i++){
+      let splitted = stringSplit(times[i]);
+      if(!splitted) return ERROR.INVALID_INPUT;
+      ms += timeframes[splitted.tf] ? splitted.t * timeframes[splitted.tf] : false;
+    }
+  }
+
   return ms;
 }
 
-function stringSplit(string){
+function stringSplit(time){
 
   let t;
   let tf;
 
-  for(let i = 0; i < string.length; i ++){
-    if(!parseInt(string[i] + 1)){
-      t = string.slice(0, i);
-      tf = string.slice(i, string.length);
+  for(let i = 0; i < time.length; i ++){
+    if(!parseInt(time[i] + 1)){
+      t = time.slice(0, i);
+      tf = time.slice(i, time.length);
       if(!t || !tf || !timeframes[tf]) return false;
       return { t:t, tf:tf };
     }
